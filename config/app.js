d2cdbeb151ca969e7656ddaae010b31e2379b7f8
postbird <ptbird@yeah.net>
2018-06-23 03:50:31 +0800

    修复 session 错误 ，增加 css/js/vendor 及 app.css 和 js.css

--- config/app.js
+++ config/app.js
@@ -9,9 +9,9 @@ module.exports = {
   session:{
     key: 'koa:sess',   //cookie key (default is koa:sess)
     maxAge: 86400000,  // cookie的过期时间 maxAge in ms (default is 1 days)
-    overwrite: true,  //是否可以overwrite    (默认default true)
-    httpOnly: true, //cookie是否只有服务器端可以访问 httpOnly or not (default true)
-    signed: true,   //签名默认true
+    overwrite: false,  //是否可以overwrite    (默认default true)
+    httpOnly: false, //cookie是否只有服务器端可以访问 httpOnly or not (default true)
+    signed: false,   //签名默认true
     rolling: false,  //在每次请求时强行设置cookie，这将重置cookie过期时间（默认：false）
     renew: false,  //(boolean) renew session when session is nearly expired,
   },
